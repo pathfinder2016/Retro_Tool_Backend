2d5585c3ffa5493bce6e381b1e3e13ad4cb45816
XIEMI2 <mike.xie@oocl.com>
2018-07-08 12:07:14 +0800

    Spring Data JPA POC

--- src/main/java/com/retro/common/model/User.java
+++ src/main/java/com/retro/common/model/User.java
@@ -1,9 +1,38 @@
 package com.retro.common.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "DOMAIN_ID")
+    private String domainId;
+    @Column(name = "DISPLAY_NAME")
+    private String displayName;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDomainId() {
+        return domainId;
+    }
+
+    public void setDomainId(String domainId) {
+        this.domainId = domainId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
