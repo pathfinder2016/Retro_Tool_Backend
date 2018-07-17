b94a5da99802c9f38ddecf19160525999c87d44b
XIEMI2 <mike.xie@oocl.com>
2018-07-17 22:07:39 +0800

    Introduce websocket POC

--- src/main/java/com/retro/core/retro/controller/CardController.java
+++ src/main/java/com/retro/core/retro/controller/CardController.java
@@ -2,6 +2,7 @@ package com.retro.core.retro.controller;
 
 import com.retro.core.retro.model.Card;
 import com.retro.core.retro.service.CardService;
+import com.retro.core.websocket.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,13 +25,17 @@ public class CardController {
 
     @Autowired CardService cardService;
 
-    @RequestMapping(value = "/public/well", method = POST)
-    public ResponseEntity upsertPublicWellCards(@RequestBody ArrayList<Card> cards){
+    @RequestMapping(value = "/public/well", method = POST, consumes = "application/json")
+    public ResponseEntity upsertPublicWellCards(@RequestBody ArrayList<Card> cards) throws IOException {
         cardService.upsertPublicWellCards(cards);
+
+        WebSocketServer.sendInfo("有新客户呼入,sltAccountId:");
+
+
         return new ResponseEntity(cards, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/all", method = GET)
+    @RequestMapping(value = "/all", method = GET, consumes = "application/json")
     public ResponseEntity findAll(){
         List<Card> cards = cardService.findAll();
         return new ResponseEntity(cards, HttpStatus.OK);
