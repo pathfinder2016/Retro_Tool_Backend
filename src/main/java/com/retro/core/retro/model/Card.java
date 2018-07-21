8ccd84ab0923a040817574cc27817afa9eab8b09
XIEMI2 <mike.xie@oocl.com>
2018-07-21 20:48:02 +0800

    Remove useless code

--- src/main/java/com/retro/core/retro/model/Card.java
+++ src/main/java/com/retro/core/retro/model/Card.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.domain.AbstractAuditable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.Objects;
 
 @Entity @Table(name = "CARD")
 public class Card extends AbstractAuditable<User, Long> {
@@ -53,26 +52,5 @@ public class Card extends AbstractAuditable<User, Long> {
 
     public void setOrder(int order) {
         this.order = order;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Card)) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-        Card card = (Card)o;
-        return Objects.equals(getContent(), card.getContent()) && Objects.equals(getType(), card.getType()) && Objects.equals(asPrivate, card.asPrivate) && Objects.equals(getOrder(), card.getOrder());
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(super.hashCode(), getContent(), getType(), asPrivate, getOrder());
     }
 }
